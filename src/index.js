deb6c3f068e6a8f4347446db1f3ef29ec40ef3a7
Sarvesh Patil <patilsarvesh1999@gmail.com>
2020-04-24 20:40:49 +0530

    c

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import ReactDom from "react-dom";
+//var Copyright = require('react-copyright');
 import "./assets/style.css";
 import quizService from "./quizService";
 import QuestionBox from "./components/QuestionBox";
@@ -65,3 +66,5 @@ class Quiz extends Component {
 }
 
 ReactDom.render(<Quiz/>, document.getElementById("root"));
+
+ReactDom.render(document.getElementById("foot"))
